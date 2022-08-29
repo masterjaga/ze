e29567cbc2b78937e7cf9bcdabd417acd3b32c10
RaghuRamAnnamdasu <annamdasuraghuram@gmail.com>
2022-08-29 10:33:56 +0530

    changes in routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import {Route, Routes, useLocation} from "react-router-dom";
+import {Route, Routes, useLocation, Navigate} from "react-router-dom";
 import { Dashboard } from './containers/Dashboard';
 import { SideNavbar } from './containers/SideNavbar';
 import { Login } from './containers/Login';
@@ -11,31 +11,52 @@ import { Classes } from './containers/Classes';
 import { Tasks } from './containers/Tasks';
 import { TopNavBar } from './containers/TopNavBar';
 
-
 function App() {
   const location = useLocation();
   const isLoginPage = location.pathname === "/" || location.pathname === "/login" || location.pathname == "/signup";
   // console.log(location);
+
+  // var userDetails = localStorage.getItem("user");
+  // userDetails = userDetails && JSON.parse(userDetails);
+  // const userName = userDetails.userName;
   return (
-    <div className={!isLoginPage ? "appContainer" : ""}>
-      {!isLoginPage && <TopNavBar />}
-      <div className={!isLoginPage ? "bodyContainer" : "loginContainer"}>
-        {!isLoginPage && <SideNavbar />}
+    <div className={!isLoginPage && localStorage.getItem("user") ? "appContainer" : ""}>
+      {!isLoginPage && localStorage.getItem("user") && <TopNavBar />}
+      <div className={!isLoginPage && localStorage.getItem("user") ? "bodyContainer" : "loginContainer"}>
+        {!isLoginPage && localStorage.getItem("user") && <SideNavbar />}
         <Routes>
-          <Route path="/" element={<Login />} />
+          <Route path="/" element={<SignUp />} />
           <Route path = "/login" element = {<Login/>} />
           <Route path = "/signup" element = {<SignUp/>} />
-          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path= "/dashboard" element={<Dashboard />} />
           <Route path = "/courses" element = {<Courses />} />
           <Route path = "/courses/:courseId" element = {<CoursePage />} />
           <Route path = "/classes" element = {<Classes />} />
           <Route path = "/tasks" element = {<Tasks />} />
+          {/* <ProtectedRoutes path = "/dashboard" element = {<Dashboard />} />
+          <ProtectedRoutes path = "/courses" element = {<Courses />} />
+          <ProtectedRoutes path = "/courses/:courseId" element = {<CoursePage />} />
+          <ProtectedRoutes path = "/classes" element = {<Classes />} />
+          <ProtectedRoutes path = "/tasks" element = {<Tasks />} /> */}
         </Routes>
       </div>
     </div>   
   );
 }
 
+
+function ProtectedRoutes({path,element}){
+
+  // var userDetails = localStorage.getItem("user");
+  // userDetails = userDetails && JSON.parse(userDetails);
+  // const userName = userDetails.userName;
+
+ return (
+  <>
+    {localStorage.getItem("user") ? <Route path = {path} element = {element} /> : <Navigate to ="/login" /> }
+  </>
+  )
+}
 
 
 export default App;
--- src/containers/TopNavBar/index.js
+++ src/containers/TopNavBar/index.js
@@ -3,17 +3,29 @@ import { useNavigate } from 'react-router-dom';
 import PowerSettingsNewIcon from '@mui/icons-material/PowerSettingsNew';
 
 export function TopNavBar() {
-  const navigate = useNavigate();
-  var user = "John John";
+    const navigate = useNavigate();
+  
+    var userDetails = localStorage.getItem("user");
+    userDetails = userDetails && JSON.parse(userDetails);
+    const userName = userDetails.userName;
+    // console.log(userDetails);
+
+
+    const signout = ()=>{
+    localStorage.clear();
+    navigate("/login");
+
+
+}
   return (
     <div className="topNavContainer">
         <div className="logo">LearnInfinity</div>
         <div className="userProfile">
             <div className="userWrapper">
-                <div className="profileImage">{user.charAt(0)}</div>
-                <div className="name">John John</div>
+                <div className="profileImage">{userName.charAt(0)}</div>
+                <div className="name">{userName}</div>
             </div>
-            <div className="signOut">
+            <div className="signOut" onClick = {signout}>
                 <PowerSettingsNewIcon />
                 {/* <img src="https://cdn1.iconfinder.com/data/icons/basic-ui-elements-coloricon/21/46-512.png"/> */}
             </div>
