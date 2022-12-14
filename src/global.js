8dc6e9112394dd007fc641190e1afc0cc253f223
RaghuRamAnnamdasu <annamdasuraghuram@gmail.com>
2022-12-14 23:31:52 +0530

    added render deployed backend url

--- src/global.js
+++ src/global.js
@@ -1,4 +1,5 @@
 // export const API = "http://localhost:4000";
 
-export const API = "https://capstone-student-dashboard.herokuapp.com"
+// export const API = "https://capstone-student-dashboard.herokuapp.com"
 
+export const API = "https://student-dashboard-backend.onrender.com";
